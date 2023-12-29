417e3a3f5d286e6dd2f71c776b3216211ad14f6a
Robert P. <porobertdev@gmail.com>
2023-12-29 21:22:03 +0200

    Use Prototypal Inheritance
    * Duh, it's part of the assignment after all.
    * Will see if I can find a use for a constructor as well.

--- script.js
+++ script.js
@@ -83,71 +83,77 @@ const api = {
 
         for (book of books) {
             if (books == userBooks.favorites) console.log('parsing favorites...');
-            
-            let html = `<img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg?default=false">
-                        <div class="info">
-                            <p class="title">${book.title}</p>
-                            <p class="author">${book.author_name}</p>
-                        </div>`;
-            let resultDiv = document.createElement('div');
-            resultDiv.classList.add('result');
-            resultDiv.innerHTML = html;
-            bookContainer.appendChild(resultDiv);
 
             let olid = book.key.split('/')[2];
 
-            // create a modal for each result
-            let modal = `<div id="modal">
-                            <div class="left">
-                                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg?default=false">
-                                <div class="links">
-                                    <a href="https://goodreads.com/book/show/${book.id_goodreads}">
-                                        <img class="goodreads" src="./assets/icons/goodreads.svg">
-                                    </a>
-                                    <a href="https://www.amazon.com/dp/${book.id_amazon}">
-                                        <img class="amazon" src="./assets/icons/amazon.svg">
-                                    </a>
-                                    <img class="read_status" src="./assets/icons/read_status.svg">
-                                    <img class="favorites ${olid}" src="./assets/icons/favorite.svg">
+            // create result & modal HTML for each book
+            Object.getPrototypeOf(book).createHTML = function() {
+                this.html = `<img src="https://covers.openlibrary.org/b/id/${this.cover_i}-L.jpg?default=false">
+                            <div class="info">
+                                <p class="title">${this.title}</p>
+                                <p class="author">${this.author_name}</p>
+                            </div>`;
+                // create a modal for each result
+                this.modal = `<div id="modal">
+                                <div class="left">
+                                    <img src="https://covers.openlibrary.org/b/id/${this.cover_i}-L.jpg?default=false">
+                                    <div class="links">
+                                        <a href="https://goodreads.com/book/show/${this.id_goodreads}">
+                                            <img class="goodreads" src="./assets/icons/goodreads.svg">
+                                        </a>
+                                        <a href="https://www.amazon.com/dp/${this.id_amazon}">
+                                            <img class="amazon" src="./assets/icons/amazon.svg">
+                                        </a>
+                                        <img class="read_status" src="./assets/icons/read_status.svg">
+                                        <img class="favorites ${olid}" src="./assets/icons/favorite.svg">
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="right">
-                                <h1 class="title">${book.title}</h1>
-                                <div class="author">
-                                    <img src='https://covers.openlibrary.org/a/olid/${book.author_key}-L.jpg'>
-                                    <p>${book.author_name}</p>
-                                </div>
-                                <div class="rating container">
-                                    <span class="rating">★</span>
-                                    <span class="rating">★</span>
-                                    <span class="rating">★</span>
-                                    <span class="rating">★</span>
-                                    <span class="rating">★</span>
-                                    <!-- <span>${book.ratings_average}</span> -->
-                                </div>
-                                <p class="description">
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat unde odio incidunt quidem culpa commodi molestias, harum, aliquid quasi nemo voluptatem eligendi? Vel eius aut magnam facere fugiat at accusamus.
+                                <div class="right">
+                                    <h1 class="title">${this.title}</h1>
+                                    <div class="author">
+                                        <img src='https://covers.openlibrary.org/a/olid/${this.author_key}-L.jpg'>
+                                        <p>${this.author_name}</p>
+                                    </div>
+                                    <div class="rating container">
+                                        <span class="rating">★</span>
+                                        <span class="rating">★</span>
+                                        <span class="rating">★</span>
+                                        <span class="rating">★</span>
+                                        <span class="rating">★</span>
+                                        <!-- <span>${this.ratings_average}</span> -->
+                                    </div>
+                                    <p class="description">
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat unde odio incidunt quidem culpa commodi molestias, harum, aliquid quasi nemo voluptatem eligendi? Vel eius aut magnam facere fugiat at accusamus.
+                                        </p>
+                                        <p>
+                                            Odit debitis a soluta nobis accusamus error ab maxime tenetur numquam illo. Officia odio inventore, corporis rem nam ab eligendi facilis animi ad exercitationem impedit temporibus adipisci ut est accusamus.
+                                        </p>
+                                        
+                                        <p>
+                                            Obcaecati, est qui molestiae esse ad explicabo eius atque assumenda, et asperiores maxime consequatur, libero quis distinctio? Dicta hic tempore animi assumenda ratione placeat veritatis, voluptates excepturi sunt inventore quia.
+                                        </p>
                                     </p>
-                                    <p>
-                                        Odit debitis a soluta nobis accusamus error ab maxime tenetur numquam illo. Officia odio inventore, corporis rem nam ab eligendi facilis animi ad exercitationem impedit temporibus adipisci ut est accusamus.
-                                    </p>
-                                    
-                                    <p>
-                                        Obcaecati, est qui molestiae esse ad explicabo eius atque assumenda, et asperiores maxime consequatur, libero quis distinctio? Dicta hic tempore animi assumenda ratione placeat veritatis, voluptates excepturi sunt inventore quia.
-                                    </p>
-                                </p>
 
-                                <div class="tags">
+                                    <div class="tags">
+                                    </div>
                                 </div>
-                            </div>
-                        </div>`;
+                            </div>`;
+
+            }
+
+            book.createHTML();
+
+            let resultDiv = document.createElement('div');
+            resultDiv.classList.add('result');
+            resultDiv.innerHTML = book.html;
+            bookContainer.appendChild(resultDiv);
 
             let dialog = document.createElement('dialog');
             let dialogAttr = book.key.split('/')[2]; // used to select only this book's stars
             dialog.classList.add(dialogAttr);
-            dialog.innerHTML = modal;
+            dialog.innerHTML = book.modal;
             resultDiv.appendChild(dialog);
 
             // @TODO: provide an alternative way to close the modal
